b7c7ebe6ae828e64f187b5c839b9c16301dad23d
Alexander Bachschmid <info@100-designs.com>
2020-03-26 18:34:14 +0100

    Live Fixes

--- resources/views/components/styles.blade.php
+++ resources/views/components/styles.blade.php
@@ -4,19 +4,19 @@
 @if(App::environment('production'))
 	<link href="{{ mix('assets/css/combined.min.css') }}" rel="stylesheet" type="text/css">
 @else
-    <link rel="stylesheet" href="{{ asset('assets/css/bundle/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/bundle/themify-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/bundle/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/bundle/animate.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/bundle/cubeportfolio.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/bundle/jquery.fancybox.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/bundle/navigation.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/bundle/settings.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/bundle/swiper.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/bundle/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/bundle/owl.theme.default.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/bundle/before-after-img.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/bundle/style.css') }}">
-    {{-- <link rel="stylesheet" href="{{ asset('assets/css/bundle/yellow.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/cubeportfolio.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/jquery.fancybox.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/navigation.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/settings.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/swiper.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/before-after-img.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('assets/css/yellow.css') }}"> --}}
     <link rel="stylesheet" href="{{ asset('dist/css/custom.css') }}">
 @endif
