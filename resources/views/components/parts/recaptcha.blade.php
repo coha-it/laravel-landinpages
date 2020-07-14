5c88675b53957382f302d8868bf6654ba2165e01
Alexander Bachschmid <info@100-designs.com>
2020-07-14 19:03:14 +0200

    Blade to pug by token and recaptcha

--- resources/views/components/parts/recaptcha.blade.php
+++
@@ -1,7 +0,0 @@
-<div
-    data-recaptcha
-    data-sitekey="{{ env('GOOGLE_RECAPTCHA_KEY') }}"
-    data-size='invisible'
-    data-badge='bottomleft'
-></div>
-{{-- <input type="hidden" name="g-recaptcha-response" value="{{ env('GOOGLE_RECAPTCHA_KEY') }}" /> --}}
