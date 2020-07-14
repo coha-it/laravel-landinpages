d1914bf138c53fdd6b80bbbe288814a410e70306
Alexander Bachschmid <info@100-designs.com>
2020-07-14 20:13:45 +0200

    "Request Offer" Button Animation

--- public/js/public_custom.js
+++ public/js/public_custom.js
@@ -1,16 +1,34 @@
-// function submitForm (e) {
-//     let form    = $(e);
-//     let data    = form.serialize();
-//     let url     = form.attr('action');
-//     let method  = form.attr('method');
+var coha_click_contact_form = function() {
+    let $ = jQuery;
 
-//     console.log(data);
+    // E-Mail Vars
+    let input = $('.email-input').first();
+    let input_height = input.height();
+    let input_top = input.offset().top;
 
-//     $.ajax({
-//         url: url,
-//         method: method,
-//         data: data
-//     });
+    // Window Vars
+    let window_height = $(window).height();
+    let window_half_height = window_height / 2;
 
-//     // return form.preventDefault();
-// }
+    // Animation Class
+    let anim_class = 'coha_email_animated';
+
+    // Body
+    let body = $("html, body");
+
+    // Run!
+    input.removeClass(anim_class);
+
+    body.stop().animate(
+        {
+            scrollTop: input_top + input_height - window_half_height
+        },
+        500,
+        'swing',
+        function() {
+            input.addClass(anim_class);
+            input.focus();
+        }
+    );
+
+}
