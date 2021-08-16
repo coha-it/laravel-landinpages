3b45778ba6c972f47f2dc6fcd00830fa6603657b
Your Name <you@example.com>
2021-08-16 19:22:49 +0200

    mini changes

--- resources/views/components/loader.blade.php
+++ resources/views/components/loader.blade.php
@@ -1,6 +1,6 @@
 <div class="loader">
-    <div class="loading-animation"></div>
-  </div>
-  <div class="loader">
-    <div class="loading-animation"></div>
-  </div>
+  <div class="loading-animation"></div>
+</div>
+<div class="loader">
+  <div class="loading-animation"></div>
+</div>
